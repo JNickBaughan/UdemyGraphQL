fc9172f7f059415962f8a077108fc5bf5d0fb01d
jbaughan <jbaughan@costar.com>
2017-10-02 12:56:32 -0400

    added companies to mock database

--- schema/schema.js
+++ schema/schema.js
@@ -3,12 +3,29 @@ const axios = require("axios");
 
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+});
+
 const UserType = new GraphQLObjectType({
   name: "User",
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(res => res.data);
+      }
+    }
   }
 });
 
@@ -24,9 +41,13 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return axios.get(`http://localhost:3000/users/${args.id}`)  
-        .then(res => res.data)
-        .then(data => { console.log(data); return data; });
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then(res => res.data)
+          .then(data => {
+            console.log(data);
+            return data;
+          });
       }
     }
   }
